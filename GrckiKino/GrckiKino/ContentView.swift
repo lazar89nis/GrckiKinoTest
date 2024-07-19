bc404c42e22d0c341aef2de63b0be0d06bb30d6a
Lazar Djordjevic <lazar89nis@gmail.com>
2024-07-19 13:06:43 +0200

    empty project

---
+++ GrckiKino/GrckiKino/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GrckiKino
+//
+//  Created by Lazar Djordjevic on 19.7.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GrckiKino/GrckiKino/GrckiKinoApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrckiKinoApp.swift
+//  GrckiKino
+//
+//  Created by Lazar Djordjevic on 19.7.24..
+//
+
+import SwiftUI
+
+@main
+struct GrckiKinoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
